3f94733a401b7de547baccf19c6d85eca81515aa
sumAllie <30742448+sumAllie@users.noreply.github.com>
2018-11-13 13:42:52 +0800

    Add files via upload

---
+++ Calculator/src/Compiler/ANS/Answer.java
@@ -0,0 +1,84 @@
+package Compiler.ANS;
+
+import Compiler.PAR.Parser;
+import Compiler.PAR.Node;
+
+import java.io.BufferedReader;
+import java.util.List;
+
+public class Answer {
+
+
+    public static String AOutput = "";
+
+
+
+    private static double calc(Node node) {
+        double val = -2222222;
+        double val1, val2;
+
+        if (null == node) {
+            AOutput += "<Error>calc: syntax error.\n";
+            return -1;
+        }
+
+        switch (node.getToken().getSymIndex()) {
+            /*数字属性节点直接返回值*/
+            case 0:
+                val = Double.valueOf(node.getToken().getToken());
+                break;
+            case 1:
+                val = Double.valueOf(node.getToken().getToken());
+                break;
+
+		/*操作符属性节点值需要先计算两个操作数的值，
+			再根据操作符来计算最后的结果*/
+            case 2:
+                val1 = calc(node.getChildNode1());
+                val2 = calc(node.getChildNode2());
+                val = val1 + val2;
+                break;
+            case 3:
+                val1 = calc(node.getChildNode1());
+                val2 = calc(node.getChildNode2());
+                val = val1 - val2;
+                break;
+            case 4:
+                val1 = calc(node.getChildNode1());
+                val2 = calc(node.getChildNode2());
+                val = val1 * val2;
+                break;
+            case 5:
+                val1 = calc(node.getChildNode1());
+                val2 = calc(node.getChildNode2());
+                val = val1 / val2;
+                break;
+            default:
+                AOutput += "<Error>cal: Unknown operation.\n";
+                break;
+        }
+        return val;
+    }
+
+    public static void AStart(BufferedReader br){
+        List<Node> nodeList = Parser.PStart(br);
+
+        int i = 0;
+        AOutput = "";
+        AOutput = AOutput + "----语义结果----\n";
+
+        for (Node node: nodeList) {
+            AOutput =  AOutput +  "第" + (++i) + "行算数式分析结果：" +  "\n";
+
+            double ans = calc(node);
+            int a = (new Double(ans)).intValue();
+            if(a == ans){
+                AOutput += a + "\n";
+            }
+            else {
+                AOutput += ans + "\n";
+            }
+        }
+    }
+
+}
---
+++ Calculator/src/Compiler/PAR/Parser.java
@@ -0,0 +1,160 @@
+package Compiler.PAR;
+import Compiler.LEX.Lexer;
+import Compiler.LEX.Token;
+import java.io.BufferedReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Parser {
+
+    private Token currToken = null;
+    public static String POutput = "";
+    static int j = -1;//
+
+    public static List<List<Token>> lexerList = new ArrayList<>();
+
+    public static List<Token> tokenList = new ArrayList<>();
+
+    private Node parse(List<Token> tList) {
+        Node node = new Node();
+        tokenList = tList;
+
+        if (j < tokenList.size() - 1 && tokenList.get(++j).getSymIndex() != -1) {
+            currToken = tokenList.get(j);
+            node = exp();
+        }
+        if (currToken == null) {
+            throw new RuntimeException("syntax error at " + currToken.toString());
+        }
+        return node;
+    }
+
+    private Node factor() {
+        Node node = new Node();
+
+        if (j < tokenList.size() - 1 && (currToken.getSymIndex() == 0 || currToken.getSymIndex() == 1)) {
+            node.setNtIndex(NodeType.FACTOR.ordinal());
+            node.setToken(currToken);
+            currToken = tokenList.get(++j);
+        } else if (j == tokenList.size() - 1 && (currToken.getSymIndex() == 0|| currToken.getSymIndex() == 1)) {
+            node.setNtIndex(NodeType.FACTOR.ordinal());
+            node.setToken(currToken);
+        } else if (j < tokenList.size() - 1 && currToken.getSymIndex() == 6) {
+            currToken = tokenList.get(++j);
+            node = exp();
+            if (j < tokenList.size() - 1 && currToken.getSymIndex() == 7) {
+                currToken = tokenList.get(++j);
+            } else if (j == tokenList.size() - 1 && currToken.getSymIndex() == 7) {
+            } else {
+                POutput = POutput + "Error : the delimeter is not matched.\n";
+                currToken = tokenList.get(++j);
+            }
+        }
+
+        return node;
+    }
+
+    private Node term() {
+        Node lnode = new Node();
+        Node rnode;
+        Node node = factor();
+
+        while (((currToken.getSymIndex() == 4) || (currToken.getSymIndex() == 5)) && j < tokenList.size() - 1) {
+            lnode.childNode1 = node.childNode1;
+            lnode.childNode2 = node.childNode2;
+            lnode.ntIndex = node.ntIndex;
+            lnode.token = node.token;
+
+            node.setChildNode1(lnode);
+
+            lnode = new Node();
+
+            node.setNtIndex(NodeType.TERM.ordinal());
+            node.setToken(currToken);
+            currToken = tokenList.get(++j);
+
+            rnode = term();
+            node.setChildNode2(rnode);
+        }
+        return node;
+    }
+
+    private Node exp() {
+        Node lnode = new Node();
+        Node rnode;
+        Node node = term();
+
+        while (((currToken.getSymIndex() == 2) || (currToken.getSymIndex() == 3)) && j < tokenList.size() - 1) {
+
+            lnode.childNode1 = node.childNode1;
+            lnode.childNode2 = node.childNode2;
+            lnode.ntIndex = node.ntIndex;
+            lnode.token = node.token;
+
+            node.setChildNode1(lnode);
+
+            lnode = new Node();
+
+            node.setNtIndex(NodeType.EXP.ordinal());
+            node.setToken(currToken);
+            currToken = tokenList.get(++j);
+
+            rnode = term();
+            node.setChildNode2(rnode);
+        }
+        return node;
+    }
+
+    public static void printTree(Node root) {
+        printInOrder(root, 0, 4);
+        POutput += "\n";
+    }
+
+    public static void printInOrder(Node node, int height,int len) {
+        if (node == null) {
+            return;
+        }
+
+        printInOrder(node.childNode1, height + 1,  len);
+        String val = node.getToken().getToken();
+        POutput += getSpace(height * len) + val + "\n";
+        printInOrder(node.childNode2, height + 1,  len);
+    }
+
+    public static String getSpace(int num) {
+        String space = " ";
+        StringBuffer buf = new StringBuffer("");
+        for (int i = 0; i < num; i++) {
+            buf.append(space);
+        }
+        return buf.toString();
+    }
+
+
+
+    public static List<Node> PStart(BufferedReader br){
+        Parser parser = new Parser();
+        lexerList = Lexer.LStart(br);
+        Node node;
+        List<Node> nodeList = new ArrayList<>();
+
+        POutput = "";
+
+        int i = 0;
+        POutput = POutput + "----语法分析----\n";
+
+        for (List<Token> tlist: lexerList) {
+            POutput =  POutput +  "第" + (++i) + "行算数式分析结果：" +  "\n";
+            node = parser.parse(tlist);
+            nodeList.add(node);
+            parser.printTree(node);
+            tlist.clear();
+            POutput = POutput + "";
+            j = -1;
+
+        }
+        lexerList.clear();
+        return nodeList;
+    }
+
+}
